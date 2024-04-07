ca25f67a8745dd6ba778a79f9b428f46a843b2a5
rajdavid <david.raj@techchefz.com>
2024-04-07 21:16:33 +0530

    event loop

---
+++ event_loop_02/event.js
@@ -0,0 +1,15 @@
+
+// Event Loop
+// 1. Node.js is single-threaded
+// 2. Node.js uses non-blocking I/O operations
+// 3. Node.js uses events and callbacks
+//  - Event-driven programming
+//  - Event Loop
+//  - Event Emitter
+//  - Event Handler
+//   - Event Listener
+//      - Event Binding
+//      - Event Registration
+//     
+
+// chunking 
